43f47b0ce2ad515184a2020274f7a129e129c2a9
Yuvraj Singh <63182105+yuvithakur007@users.noreply.github.com>
2023-01-05 20:52:59 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (66.12%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,25 +14,24 @@
  * }
  */
 public class Solution {
-    int ans=0;
+    int diameter = 0;
+
     public int diameterOfBinaryTree(TreeNode root) {
-        if(root==null )return 0;
-        height(root);
-        return ans;
+        longestPath(root);
+        return diameter;
     }
-    
-    public int height(TreeNode root){
-        //if root==null height==0
-        if(root==null)return -1;
-        
-        int L=height(root.left);
-        int R=height(root.right);
-        //ans signfies(no. of nodes farthest apart) or the DIAMETER
-        ans=Math.max(ans,L+R+2);
-        //height of the tree is max of LST & RST +1
-        return 1+Math.max(L,R);
+
+    private int longestPath(TreeNode node) {
+        if (node == null) {
+            return 0;
+        } else {
+            int leftDepth = longestPath(node.left);
+            int rightDepth = longestPath(node.right);
+            diameter = Math.max(diameter, leftDepth + rightDepth); 
+            // it is not leftDepth + rightDepth+1 bcoz we are not counting nodes but path
+            return 1 + Math.max(leftDepth, rightDepth);
+        }
     }
-    
     
 //     public int diameterOfBinaryTree(TreeNode root) {
 //         int[] diameter = new int[1];
