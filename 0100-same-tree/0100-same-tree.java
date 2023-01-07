08b0bb4285f8e3980bd78926bfbbdf03a4b3dff9
Yuvraj Singh <63182105+yuvithakur007@users.noreply.github.com>
2023-01-07 21:58:37 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (77.30%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,21 +14,21 @@
  * }
  */
 class Solution {
-        public boolean isSameTree(TreeNode node1, TreeNode node2) {
-        if (node1 == null && node2 == null)
-            return true;
-        else if (node1 == null || node2 == null)
-            return false;
+//         public boolean isSameTree(TreeNode node1, TreeNode node2) {
+//         if (node1 == null && node2 == null)
+//             return true;
+//         else if (node1 == null || node2 == null)
+//             return false;
 
-        return ((node1.val == node2.val) && isSameTree(node1.left, node2.left) && isSameTree(node1.right, node2.right));
+//         return ((node1.val == node2.val) && isSameTree(node1.left, node2.left) && isSameTree(node1.right, node2.right));
+//     }
+    
+    
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // recurision method
+        if (p == null && q == null) return true;
+        if (p == null && q != null || p != null && q == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
-    
-    
-    // public boolean isSameTree(TreeNode p, TreeNode q) {
-    //     // recurision method
-    //     if (p == null && q == null) return true;
-    //     if (p == null && q != null || p != null && q == null) return false;
-    //     if (p.val != q.val) return false;
-    //     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-    // }
 }
