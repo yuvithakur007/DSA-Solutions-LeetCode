aaf17d9a8c126820e52ea4f723bd9d0f647787e7
Yuvraj Singh <63182105+yuvithakur007@users.noreply.github.com>
2023-01-08 19:17:48 +0530

    Time: 1 ms (93.05%), Space: 41.1 MB (74.26%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) 
+            return res;
+        
+        //The offer() operation is used to insert new element into the queue. If it performs insert operation successfully, it returns “true” value. Otherwise it returns “false” value.
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root); 
+        int level = 0; // the number of level !!
+        while(!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int size = queue.size();
+            for(int i=0; i<size; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+            if(level % 2 == 1) { //reverse data
+                Collections.reverse(list);
+            }
+            res.add(list);
+
+            level++; // after traverse all member of element in same level, go next level
+        }
+        return res;
+    }
+}
