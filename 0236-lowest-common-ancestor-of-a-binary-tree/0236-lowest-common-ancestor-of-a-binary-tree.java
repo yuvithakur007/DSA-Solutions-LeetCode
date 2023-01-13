81bb70f940116a8fa83eda6f2b74eb117ee8d3eb
Yuvraj Singh <63182105+yuvithakur007@users.noreply.github.com>
2023-01-13 23:28:36 +0530

    Time: 7 ms (71.02%), Space: 44 MB (59.44%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -11,12 +11,12 @@ class Solution {
         public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         //base case
         if (root == null || root == p || root == q) {
+            // found p or q or touch the ground
             return root;
-        }
+        } // search p and q from left and right
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
-
-        //result
+        // from root's left & right we found both p and q, so root is the LCA
         if(left == null) {
             return right;
         }
@@ -27,22 +27,4 @@ class Solution {
             return root;
         }
     }
-//        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-//       if (root == null || root == p || root == q)
-//         // found p or q or touch the ground
-//         return root;
-
-//       // search p and q from left and right
-//       TreeNode left = lowestCommonAncestor(root.left, p, q);
-//       TreeNode right = lowestCommonAncestor(root.right, p, q);
-
-//       if (left != null && right != null)
-//         // from root's left & right we found both p and q, so root is the LCA
-//         return root;
-//       else
-//         // left is not null means from left's left & right we found both q and q
-//         // so left is the LCA, otherwise, right is the answer
-//         return left != null ? left : right;
-//     }
-    
 }
