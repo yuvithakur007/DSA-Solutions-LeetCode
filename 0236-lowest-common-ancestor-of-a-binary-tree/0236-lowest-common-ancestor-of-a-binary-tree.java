fa4ef177b8334727c602671c3324d55d99742596
Yuvraj Singh <63182105+yuvithakur007@users.noreply.github.com>
2023-01-13 23:22:34 +0530

    Time: 7 ms (71.02%), Space: 43.7 MB (79.25%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,21 +8,41 @@
  * }
  */
 class Solution {
-       public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-      if (root == null || root == p || root == q)
-        // found p or q or touch the ground
-        return root;
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //base case
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-      // search p and q from left and right
-      TreeNode left = lowestCommonAncestor(root.left, p, q);
-      TreeNode right = lowestCommonAncestor(root.right, p, q);
-
-      if (left != null && right != null)
-        // from root's left & right we found both p and q, so root is the LCA
-        return root;
-      else
-        // left is not null means from left's left & right we found both q and q
-        // so left is the LCA, otherwise, right is the answer
-        return left != null ? left : right;
+        //result
+        if(left == null) {
+            return right;
+        }
+        else if(right == null) {
+            return left;
+        }
+        else { //both left and right are not null, we found our result
+            return root;
+        }
     }
+//        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//       if (root == null || root == p || root == q)
+//         // found p or q or touch the ground
+//         return root;
+
+//       // search p and q from left and right
+//       TreeNode left = lowestCommonAncestor(root.left, p, q);
+//       TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+//       if (left != null && right != null)
+//         // from root's left & right we found both p and q, so root is the LCA
+//         return root;
+//       else
+//         // left is not null means from left's left & right we found both q and q
+//         // so left is the LCA, otherwise, right is the answer
+//         return left != null ? left : right;
+//     }
+    
 }
