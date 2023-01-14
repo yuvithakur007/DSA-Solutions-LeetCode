dbc21ab58ce2794c5a286ddbdd3f935d999b4402
Yuvraj Singh <63182105+yuvithakur007@users.noreply.github.com>
2023-01-14 22:11:34 +0530

    Time: 4 ms (99.96%), Space: 63.8 MB (49.05%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,21 +1,74 @@
 class Solution {
-    public int longestConsecutive(int[] nums) {
-        Set<Integer> hashSet = new HashSet<Integer>();
-        for (int num : nums) {
-            hashSet.add(num);
-        }
-        int longestStreak = 0;
-        for (int num : nums) {
-            if (!hashSet.contains(num-1)) {
-                int currentNum = num;
-                int currentStreak = 1;
-                while (hashSet.contains(currentNum+1)) {
-                    currentNum += 1;
-                    currentStreak += 1;
+     public int longestConsecutive(int[] nums) {int result = 0;
+        if (nums.length > 0) {
+            if (nums.length < 1000) {
+                Arrays.sort(nums);
+                int current = 0;
+                for (int i = 1; i < nums.length; i++) {
+                    if (nums[i] != nums[i - 1]) {
+                        if (nums[i] - nums[i - 1] == 1) {
+                            current++;
+                        } else {
+                            if (current + 1 > result) {
+                                result = current + 1;
+                            }
+                            current = 0;
+                        }
+                    }
                 }
-                longestStreak = Math.max(longestStreak, currentStreak);
+                if (current + 1 > result) {
+                    result = current + 1;
+                }
+            } else {
+                int min = Integer.MAX_VALUE;
+                int max = Integer.MIN_VALUE;
+                for (int num : nums) {
+                    if (num > max) {
+                        max = num;
+                    }
+                    if (num < min) {
+                        min = num;
+                    }
+                }
+                byte[] bits = new byte[max - min + 1];
+                for (int num : nums) {
+                    bits[num - min] = 1;
+                }
+                int current = 0;
+                for (byte bit : bits) {
+                    if (bit > 0) {
+                        current++;
+                    } else {
+                        if (current > result) {
+                            result = current;
+                        }
+                        current = 0;
+                    }
+                }
+                if (current > result) {
+                    result = current;
+                }
             }
         }
-        return longestStreak;
+        return result;
     }
+    // public int longestConsecutive(int[] nums) {
+    //     Set<Integer> hashSet = new HashSet<Integer>();
+    //     for (int num : nums) {
+    //         hashSet.add(num);
+    //     }
+    //     int longestStreak = 0;
+    //     for (int num : nums) {
+    //         if (!hashSet.contains(num-1)) {
+    //             int currentNum = num;
+    //             int currentStreak = 1;
+    //             while (hashSet.contains(currentNum+1)) {
+    //                 currentNum += 1;
+    //                 currentStreak += 1;
+    //             }
+    //             longestStreak = Math.max(longestStreak, currentStreak);
+    //         }
+    //     }
+    //     return longestStreak;
+    // }
 }
