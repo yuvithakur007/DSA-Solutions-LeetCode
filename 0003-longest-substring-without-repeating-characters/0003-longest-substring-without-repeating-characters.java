241f42f46d8840fdfe4279d7f281105560aa9006
Yuvraj Singh <01yuvisingh@gmail.com>
2023-01-15 21:57:24 +0530

    Time: 7 ms (77.68%), Space: 42.6 MB (68.72%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,18 +1,38 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-     HashMap < Character, Integer > mpp = new HashMap < Character, Integer > ();
+//      HashMap < Character, Integer > mpp = new HashMap < Character, Integer > ();
 
-        int left = 0, right = 0;
-        int n = s.length();
-        int len = 0;
-        while (right < n) {
-            if (mpp.containsKey(s.charAt(right))) left = Math.max(mpp.get(s.charAt(right)) + 1, left);
+//         int left = 0, right = 0;
+//         int n = s.length();
+//         int len = 0;
+//         while (right < n) {
+//             if (mpp.containsKey(s.charAt(right))) left = Math.max(mpp.get(s.charAt(right)) + 1, left);
 
-            mpp.put(s.charAt(right), right);
+//             mpp.put(s.charAt(right), right);
 
-            len = Math.max(len, right - left + 1);
-            right++;
+//             len = Math.max(len, right - left + 1);
+//             right++;
+//         }
+//         return len;
+//     }
+        
+         if(s.length()==0)
+             return 0;
+        int maxans = Integer.MIN_VALUE;
+        Set < Character > set = new HashSet < > ();
+        int l = 0;
+        for (int r = 0; r < s.length(); r++) // outer loop for traversing the string
+        {
+            if (set.contains(s.charAt(r))) //if duplicate element is found
+            {
+                while (l < r && set.contains(s.charAt(r))) {
+                    set.remove(s.charAt(l));
+                    l++;
+                }
+            }
+            set.add(s.charAt(r));
+            maxans = Math.max(maxans, r - l + 1);
         }
-        return len;
+        return maxans;
     }
 }
