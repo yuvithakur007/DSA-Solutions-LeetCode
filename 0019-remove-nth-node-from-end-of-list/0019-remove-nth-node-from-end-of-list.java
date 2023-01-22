19d7cfc0a189a90677b379c9292bb2a0a902341f
Yuvraj Singh <01yuvisingh@gmail.com>
2023-01-22 23:24:32 +0530

    Time: 1 ms (41.99%), Space: 42.5 MB (13.72%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,54 +10,21 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode t = head;
-        ListNode p = head;
-        int i = 0;
-        if(t.next==null) 
-            return null;
-        //count
-        while(t!=null){
-            t=t.next;
-            i++;
-        }
-        
-        //if out of index
-        if(i-n<0) 
-            return null;
-        
-        // if at start
-        if(i-n==0) {
-            p=p.next;
-            return p;
-            }
-        
-        int j = 0;
-        ListNode prev;
-        while(p!=null){
-            prev = p;
-            p=p.next;
-            if(j==(i-n-1)){
-                prev.next = p.next;
-            }
-            j++;
-        }
-        return head;
-    }
-//         ListNode start = new ListNode(0);
-//         start.next = head;
-//         ListNode slow = start, fast = start;
+        ListNode start = new ListNode(0);
+        start.next = head;
+        ListNode slow = start, fast = start;
 
-//         //Move fast in front so that the gap between slow and fast becomes n
-//         for(int i=1; i<=n+1; i++)   {
-//             fast = fast.next;
-//         }
-//         //Move fast to the end, maintaining the gap
-//         while(fast != null) {
-//             slow = slow.next;
-//             fast = fast.next;
-//         }
-//         //Skip the desired node
-//         slow.next = slow.next.next;
-//         return start.next;
-//     }
+        //Move fast in front so that the gap between slow and fast becomes n
+        for(int i=1; i<=n+1; i++)   {
+            fast = fast.next;
+        }
+        //Move fast to the end, maintaining the gap
+        while(fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        //Skip the desired node
+        slow.next = slow.next.next;
+        return start.next;
+    }
 }
