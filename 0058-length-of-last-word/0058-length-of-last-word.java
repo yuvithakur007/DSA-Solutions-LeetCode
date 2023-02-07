61b13f589b788405d74530dfda1861e30c468a34
Yuvraj Singh <01yuvisingh@gmail.com>
2023-02-07 22:40:55 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (71.97%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,14 +1,17 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int lastWordLength = 0;
-        for(int i=s.length()-1; i>=0; i--) {
-            char ch = s.charAt(i);
-            if(ch != ' ') {
-                lastWordLength++;
-            } else if(lastWordLength > 0) {
-                return lastWordLength;
-            }
-        }
-        return lastWordLength;
+         String trim = s.trim();
+        int afterLastSpaceIndex = trim.lastIndexOf(' ') + 1;
+        return trim.length() - afterLastSpaceIndex;
+        // int lastWordLength = 0;
+        // for(int i=s.length()-1; i>=0; i--) {
+        //     char ch = s.charAt(i);
+        //     if(ch != ' ') {
+        //         lastWordLength++;
+        //     } else if(lastWordLength > 0) {
+        //         return lastWordLength;
+        //     }
+        // }
+        // return lastWordLength;
     }
 }
