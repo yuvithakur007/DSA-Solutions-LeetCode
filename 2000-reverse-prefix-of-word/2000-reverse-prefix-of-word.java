cd70613ca9822560d654cc24da93535b41d1cb40
Yuvraj Singh <01yuvisingh@gmail.com>
2023-02-08 19:30:18 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (90.08%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -1,20 +1,30 @@
 class Solution { 
     public String reversePrefix(String word, char ch) {
-        char[] c = word.toCharArray();
-        int locate = 0;
-        for (int i = 0; i < word.length(); i++) { //first occurrence of ch
-            if (ch == c[i]) {
-                locate = i;
-                break;
-            }
-        }
-        char[] res = new char[word.length()];
-        for (int i = 0; i <= locate; i++) {
-            res[i] = c[locate - i];
-        }
-        for (int i = locate + 1; i < word.length(); i++) {
-            res[i] = c[i];
-        }
-        return String.valueOf(res);
+        int index = word.indexOf(ch) + 1;
+        if(index < 2)
+            return word;
+        
+        StringBuilder sb = new StringBuilder(word.substring(0, index));
+        sb.reverse();
+        sb.append(word.substring(index));
+        
+        return sb.toString();
+        
+        // char[] c = word.toCharArray();
+        // int locate = 0;
+        // for (int i = 0; i < word.length(); i++) { //first occurrence of ch
+        //     if (ch == c[i]) {
+        //         locate = i;
+        //         break;
+        //     }
+        // }
+        // char[] res = new char[word.length()];
+        // for (int i = 0; i <= locate; i++) {
+        //     res[i] = c[locate - i];
+        // }
+        // for (int i = locate + 1; i < word.length(); i++) {
+        //     res[i] = c[i];
+        // }
+        // return String.valueOf(res);
     }
 }
