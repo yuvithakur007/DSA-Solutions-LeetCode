385b01d1627a1015a2f58a6cf90e853a696e9919
Yuvraj Singh <01yuvisingh@gmail.com>
2023-02-09 20:08:04 +0530

    Time: 4 ms (85.13%), Space: 42.1 MB (97.06%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,44 +1,89 @@
 class Solution {
-     public static int romanToInt(String s) {
-        int sum = 0,temp=0;
+    public int romanToInt(String s) {
+        int nums[] = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
+                case 'M':
+                    nums[i] = 1000;
+                    break;
 
-        for (int i = s.length()-1; i >=0 ; i--) {
-            int x = value(s.charAt(i));
-            int val=x;
+                case 'D':
+                    nums[i] = 500;
+                    break;
 
-            if(val<temp){
-                sum-=val;
+                case 'C':
+                    nums[i] = 100;
+                    break;
+
+                case 'L':
+                    nums[i] = 50;
+                    break;
+
+                case 'X':
+                    nums[i] = 10;
+                    break;
+
+                case 'V':
+                    nums[i] = 5;
+                    break;
+
+                case 'I':
+                    nums[i] = 1;
+                    break;
             }
-            else{
-                sum+=val;
-            }
-             temp=x;
         }
-        return sum;
-    }
-
-    public static  int value(char x) {
-        if (x == 'I') {
-            return 1;
+        int sum = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] < nums[i + 1])
+                sum -= nums[i];
+            else
+                sum += nums[i];
         }
-        if (x == 'V') {
-            return 5;
-        }
-        if (x == 'X') {
-            return 10;
-        }
-        if (x == 'L') {
-            return 50;
-        }
-        if (x == 'C') {
-            return 100;
-        }
-        if (x == 'D') {
-            return 500;
-        }
-        if (x == 'M') {
-            return 1000;
-        }
-        return -1;
+        return sum + nums[nums.length - 1];
     }
 }
+
+// class Solution {
+//      public static int romanToInt(String s) {
+//         int sum = 0,temp=0;
+
+//         for (int i = s.length()-1; i >=0 ; i--) {
+//             int x = value(s.charAt(i));
+//             int val=x;
+
+//             if(val<temp){
+//                 sum-=val;
+//             }
+//             else{
+//                 sum+=val;
+//             }
+//              temp=x;
+//         }
+//         return sum;
+//     }
+
+//     public static  int value(char x) {
+//         if (x == 'I') {
+//             return 1;
+//         }
+//         if (x == 'V') {
+//             return 5;
+//         }
+//         if (x == 'X') {
+//             return 10;
+//         }
+//         if (x == 'L') {
+//             return 50;
+//         }
+//         if (x == 'C') {
+//             return 100;
+//         }
+//         if (x == 'D') {
+//             return 500;
+//         }
+//         if (x == 'M') {
+//             return 1000;
+//         }
+//         return -1;
+//     }
+// }
