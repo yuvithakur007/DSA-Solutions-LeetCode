eba2647960a4dd758e8c3b6f893ba410ab402be0
Yuvraj Singh <01yuvisingh@gmail.com>
2023-02-11 14:51:44 +0530

    Time: 7 ms (44.06%), Space: 40.2 MB (34.85%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -1,35 +1,35 @@
-// class Solution {
-//     public String convertToTitle(int n) {
-//       String result = "";
-       
-//        while(n > 0) {
-//            n --;
-//            result = (char)('A' + (n % 26)) + result;
-//            n = n / 26;
-//        }
-//        return result;
-//     }
-// }
-
 class Solution {
-    public String convertToTitle(int columnNumber) {
-        // Create an empty string for storing the characters...
-        StringBuilder output = new StringBuilder();
-        // Run a while loop while columnNumber is positive...
-        while(columnNumber > 0){
-            // Subtract 1 from columnNumber...
-            columnNumber--;
-            // Get current character by doing modulo of columnNumber by 26...
-            char c = (char) (columnNumber % 26 + 'A');
-            // Divide columnNumber by 26...
-            columnNumber /= 26;
-            // Append the character into output...
-            output.append(c);
-        }
-        // Reverse the output string...
-        // Bcause we have found characters from right to left...
-        output.reverse();
-        // Return the reversed string.
-        return output.toString();
+    public String convertToTitle(int n) {
+      String result = "";
+       
+       while(n > 0) {
+           n --;
+           result = (char)('A' + (n % 26)) + result;
+           n = n / 26;
+       }
+       return result;
     }
 }
+
+// class Solution {
+//     public String convertToTitle(int columnNumber) {
+//         // Create an empty string for storing the characters...
+//         StringBuilder output = new StringBuilder();
+//         // Run a while loop while columnNumber is positive...
+//         while(columnNumber > 0){
+//             // Subtract 1 from columnNumber...
+//             columnNumber--;
+//             // Get current character by doing modulo of columnNumber by 26...
+//             char c = (char) (columnNumber % 26 + 'A');
+//             // Divide columnNumber by 26...
+//             columnNumber /= 26;
+//             // Append the character into output...
+//             output.append(c);
+//         }
+//         // Reverse the output string...
+//         // Bcause we have found characters from right to left...
+//         output.reverse();
+//         // Return the reversed string.
+//         return output.toString();
+//     }
+// }
