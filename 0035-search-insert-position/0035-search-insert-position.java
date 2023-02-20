f3925fc8520420fab8b9ec8a8113ec93227d82b3
Yuvraj Singh <01yuvisingh@gmail.com>
2023-02-20 12:04:32 +0530

    Time: 0 ms (100.00%), Space: 42 MB (39.95%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,11 +1,29 @@
 class Solution {
+    //binary search O(logn)
     public int searchInsert(int[] nums, int target) {
-        int n= nums.length;
-        for(int i=0; i<n; i++){
-            if(nums[i]==target || target<nums[i]){
-                return i;
+        int lo = 0, hi = nums.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (target < nums[mid]) {
+                hi = mid - 1;
+            } else if (target > nums[mid]) {
+                lo = mid + 1;
+            } else {
+                return mid;
             }
         }
-        return n;
+
+        return lo;
     }
 }
+// linera O(n)
+//     public int searchInsert(int[] nums, int target) {
+//         int n= nums.length;
+//         for(int i=0; i<n; i++){
+//             if(nums[i]==target || target<nums[i]){
+//                 return i;
+//             }
+//         }
+//         return n;
+//     }
+// }
