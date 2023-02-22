9fad1b0ec94a1692a9011b19956d0a39b5db0aee
Yuvraj Singh <01yuvisingh@gmail.com>
2023-02-22 22:18:53 +0530

    Time: 9 ms (95.22%), Space: 54 MB (11.80%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,42 +1,36 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int max = 0;
-        int sum = 0;
-        int res = 0;
-        if(weights.length < days) res = -1;
-        for(int i = 0; i< weights.length; i++){
-            max = Math.max(max , weights[i]);
-            sum += weights[i];
+        int left = 0;
+        int right = 0;
+        for(int i :weights){
+            left = Math.max(left,i);
+            right +=i;
         }
-        int low = max ; 
-        int high = sum; 
-        int mid = 0;
-        
-        while(low <= high){
-            mid = low + (high - low)/2;
-            if(isvalid(weights, mid , days)){
-                res = mid;
-                high = mid - 1;
-                
-            }
-            else{
-                low = mid +1;
+        int mid;
+        int ans = right;
+        while(left<=right){
+            mid = (left+right)/2;
+            if(check(weights,days,mid)){
+                ans = mid;
+                right = mid-1;
+            }else{
+                left = mid+1;
             }
         }
-        return res;
+        return ans;
     }
-    public static boolean isvalid(int [] weights , int mid , int days){
-        int n = weights.length;
-        int d = 1;
-        int sum = 0;
-      for( int i = 0; i< n ; i++){
-          sum += weights[i];
-          if(sum > mid){
-              d++;
-              sum = weights[i];
-          }
-      }
-        if(d> days) return false;
+
+    public boolean check(int [] weights, int days, int capacity){
+        int requiredDays = 1;
+        int currWeight = 0;
+        for(int i:weights){
+            if(currWeight+i>capacity){
+                requiredDays++;
+                currWeight = 0;
+            }
+            currWeight +=i;
+        }
+        if(requiredDays>days) return false;
         return true;
     }
 }
