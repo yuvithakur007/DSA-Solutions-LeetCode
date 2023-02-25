66f8388e2ae511ce22648809a766aa336b6667a8
Yuvraj Singh <01yuvisingh@gmail.com>
2023-02-25 22:09:04 +0530

    Time: 2 ms (63.52%), Space: 59.8 MB (17.47%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -9,12 +9,3 @@ class Solution {
         return maxProfit;
     }
 }
-        // test cases failed
-        // int maxProfit = 0;
-        // for (int i = 0; i < arr.length; i++) {
-        //     for (int j = i + 1; j < arr.length; j++) {
-        //         if (arr[j] > arr[i]) {
-        //             maxProfit = Math.max(arr[j] - arr[i], maxProfit);
-        //         }
-        //     }
-        // }
