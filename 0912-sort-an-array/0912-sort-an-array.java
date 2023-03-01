43588f1b099b936743e9e8d4bac0495c1e2ce932
Yuvraj Singh <01yuvisingh@gmail.com>
2023-03-01 22:19:15 +0530

    Time: 30 ms (75.78%), Space: 53.3 MB (46.30%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,11 +1,46 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
-        int a[]=new int[nums.length];
-        for(int i:nums) pq.add(i);
-        for(int i=0;i<nums.length;i++){
-            a[i]=pq.poll();
+        sort(nums, 0, nums.length - 1);
+        return nums;
+    }    
+    private void sort(int[] nums, int begin, int end) {
+        if (begin >= end) {
+            return;
+        }        
+        int mid = (begin + end) / 2;        
+        //recursively sort from begin to mid
+        sort(nums, begin, mid);
+        //recursively sort from mid + 1 to end
+        sort(nums, mid + 1, end);
+        // merge two sections together
+        int[] merged = new int[end - begin + 1];
+        int left = begin;
+        int right = mid + 1;        
+        int counter = 0;
+        while (left <= mid && right <= end) {
+            if (nums[left] <= nums[right]) {
+                merged[counter++] = nums[left];
+                left++;
+            }
+            else {
+                merged[counter++] = nums[right];
+                right++;
+            }
+        }        
+        if (left > mid) {
+            for (int i = right; i <= end; i++) {
+                merged[counter++] = nums[i];
+            }
+        }        
+        if (right > end) {
+            for (int i = left; i <= mid; i++) {
+                merged[counter++] = nums[i];
+            }
         }
-        return a;
+        // copy merged back to original array
+        counter = 0;
+        for (int i = begin; i <= end; i++) {
+            nums[i] = merged[counter++];
+        }
     }
 }
