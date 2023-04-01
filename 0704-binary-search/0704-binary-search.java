7bcdae20fb83ffc40478e2af5a996bfe50d8806e
Yuvraj Singh <01yuvisingh@gmail.com>
2023-04-01 15:59:25 +0530

    Time: 0 ms (100.00%), Space: 43.6 MB (15.33%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,19 +1,30 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        return binarySearch(nums, 0, nums.length-1, target);
-    }
-    // using binary with recursion
-    public int binarySearch(int[] nums, int start, int end, int target){
-        if(start <= end){
-            int mid = start + (end - start)/2;
-            if(nums[mid] == target){
-                return mid;
-            }if(nums[mid] < target){
-                return binarySearch(nums, mid+1, end, target);
-            }
-            return binarySearch(nums, start, mid-1, target);
-        }
+// iteration
+		public int search(int[] nums, int target) {
+		int low =0,high=nums.length-1;
+		while(low<=high){
+		int mid=(low + (high-low)/2);
+		if(nums[mid]==target)return mid;
+		if(nums[mid]>target)high=mid-1;
+		if(nums[mid]<target)low=mid+1;
+		}
+		return -1;
+		}
+// using binary with recursion
+//     public int search(int[] nums, int target) {
+//         return binarySearch(nums, 0, nums.length-1, target);
+//     }
+//     public int binarySearch(int[] nums, int start, int end, int target){
+//         if(start <= end){
+//             int mid = start + (end - start)/2;
+//             if(nums[mid] == target){
+//                 return mid;
+//             }if(nums[mid] < target){
+//                 return binarySearch(nums, mid+1, end, target);
+//             }
+//             return binarySearch(nums, start, mid-1, target);
+//         }
         
-        return -1;
-    }
+//         return -1;
+//     }
 }
