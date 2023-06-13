3bd1c3a860066e32e3d7e1547985c5e1d2dc862a
Yuvraj Singh <01yuvisingh@gmail.com>
2023-06-13 09:09:30 +0530

    Time: 11 ms (90.72%), Space: 53.1 MB (9.57%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int equalPairs(int[][] grid) {
+        int n = grid.length;
+        int count = 0;
+        int trans[][] = new int[n][n];
+        transpose(grid, trans);
+//compare each row of original matrix with its transpose
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(Arrays.equals(grid[i], trans[j]))
+                    count++;
+            }
+        }
+        return count;
+    }
+    private void transpose(int A[][], int B[][]){
+        int n = A.length;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                B[i][j] = A[j][i];
+    }
+}
