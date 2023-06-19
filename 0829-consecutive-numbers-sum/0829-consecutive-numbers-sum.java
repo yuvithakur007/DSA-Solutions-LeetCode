acf97bcd974f61dc2da2b26d4d3a9def545ab9ff
Yuvraj Singh <01yuvisingh@gmail.com>
2023-06-19 16:01:27 +0530

    Time: 3 ms (92.83%), Space: 39.3 MB (76.79%) - LeetHub

---
+++ 0829-consecutive-numbers-sum/0829-consecutive-numbers-sum.java
@@ -0,0 +1,13 @@
+class Solution {
+public int consecutiveNumbersSum(int n) {
+        int count =0;
+        int i=1;
+
+        while(n>0){
+            n -=i;
+            if(n%i == 0) count++;
+            i++; 
+        }
+        return count;
+    }
+}
