50599bd86e09dbb2c70e962f0c53b04c89312f26
Yuvraj Singh <01yuvisingh@gmail.com>
2023-06-26 23:05:42 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (12.15%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -3,11 +3,13 @@ class Solution {
         int n = mat.length;
         int result = 0;
         for (int i = 0; i < n; i++) {
-            result += mat[i][i] + mat[i][n - i - 1];
+            result += mat[i][i];
+            if(i!= n-i-1)
+                result+=mat[i][n - i - 1];
         }
-        if (n % 2 == 1) {
-            result -= mat[n / 2][n / 2];
-        }
+        // if (n % 2 == 1) {
+        //     result -= mat[n / 2][n / 2];
+        // }
         return result;
     }
 }
