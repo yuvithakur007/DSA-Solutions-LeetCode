4818cc0ca5ec2ac1b39d7dcb75bfa707545a6b88
Yuvraj Singh <01yuvisingh@gmail.com>
2023-08-05 15:27:42 +0530

    Time: 1 ms (99.88%), Space: 43.5 MB (78.67%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        return helper(1, n);
+    }
+    
+    private List<TreeNode> helper(int lo, int hi) {
+        List<TreeNode> result = new ArrayList<>();
+        //base case
+        if (lo > hi) {
+            result.add(null);
+            return result;
+        }
+        
+        //subproblem to reach boootm
+        for (int i = lo; i <= hi; i++) {
+            List<TreeNode> left = helper(lo, i - 1);
+            List<TreeNode> right = helper(i + 1, hi);
+            //reconstruct tree from bottom to up
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    result.add(root);
+                }
+            }
+        }
+        //return list of root to last layer
+        return result;
+    }
+}
+//DC
