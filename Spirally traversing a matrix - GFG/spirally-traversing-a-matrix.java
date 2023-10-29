d27b2606ffd4c822e3cfe6576a314c246bab1dd9
Yuvraj Singh <01yuvisingh@gmail.com>
2023-10-30 00:46:54 +0530

    Added solution - LeetHub

--- Spirally traversing a matrix - GFG/spirally-traversing-a-matrix.java
+++ Spirally traversing a matrix - GFG/spirally-traversing-a-matrix.java
@@ -40,27 +40,20 @@ class Solution
         int top = 0, bottom = r - 1, right = c - 1, left = 0;
 
         while (top <= bottom && left <= right) {
-            // Traverse from left to right
             for (int i = left; i <= right; i++) {
                 result.add(matrix[top][i]);
             }
             top++;
-
-            // Traverse from top to bottom
             for (int i = top; i <= bottom; i++) {
                 result.add(matrix[i][right]);
             }
             right--;
-
-            // Traverse from right to left
             if (top <= bottom) {
                 for (int i = right; i >= left; i--) {
                     result.add(matrix[bottom][i]);
                 }
                 bottom--;
             }
-
-            // Traverse from bottom to top
             if (left <= right) {
                 for (int i = bottom; i >= top; i--) {
                     result.add(matrix[i][left]);
