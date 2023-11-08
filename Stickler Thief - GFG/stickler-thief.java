78f31d532a2dd0e058b6f9d6cd851c0d7a4600cf
Yuvraj Singh <01yuvisingh@gmail.com>
2023-11-08 23:50:04 +0530

    Added solution - LeetHub

---
+++ Stickler Thief - GFG/stickler-thief.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class GFG
+ {
+	public static void main (String[] args)
+	 {
+	  
+	  //taking input using Scanner class
+	  Scanner sc = new Scanner(System.in);
+	  
+	  //taking count of testcases
+	  int t = sc.nextInt();
+	  while(t-- > 0){
+	      
+	      //taking count of houses
+	      int n = sc.nextInt();
+	      int arr[] = new int[n];
+	      
+	      //inserting money present in 
+	      //each house in the array
+	      for(int i = 0; i<n; ++i)
+	           arr[i] = sc.nextInt();
+  	      
+  	      //calling method FindMaxSum() of class solve
+  	      System.out.println(new Solution().FindMaxSum(arr, n));
+	 }
+   }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the maximum money the thief can get.
+    public int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        int s1=0,s2=0;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                s1=Math.max(s1+arr[i],s2);
+            }
+            else{
+                s2=Math.max(s2+arr[i],s1);
+            }
+        }
+        if(s1>s2){
+            return s1;
+        }
+        else{
+            return s2;
+        }
+    }
+}
