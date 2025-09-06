862ba1a0f4f96ed38b191c983b9e9754ea540143
Yuvraj Singh <01yuvisingh@gmail.com>
2025-09-06 16:52:01 +0530

    Time: 5 ms (27.96%), Space: 42.3 MB (51.37%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,37 +1,20 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        List<String> res = new ArrayList<>();
-        
-        if (digits == null || digits.length() == 0) {
-            return res;
-        }
-        
-        Map<Character, String> digitToLetters = new HashMap<>();
-        digitToLetters.put('2', "abc");
-        digitToLetters.put('3', "def");
-        digitToLetters.put('4', "ghi");
-        digitToLetters.put('5', "jkl");
-        digitToLetters.put('6', "mno");
-        digitToLetters.put('7', "pqrs");
-        digitToLetters.put('8', "tuv");
-        digitToLetters.put('9', "wxyz");
-        
-        backtrack(digits, 0, new StringBuilder(), res, digitToLetters);
-        
-        return res;        
+        if(digits.isEmpty()) return Collections.emptyList();
+        String[] phone = { "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> output = new ArrayList<>();
+        backtrack("", digits, phone, output);
+        return output;
     }
 
-    private void backtrack(String digits, int idx, StringBuilder comb, List<String> res, Map<Character, String> digitToLetters) {
-        if (idx == digits.length()) {
-            res.add(comb.toString());
-            return;
+    private void backtrack(String combination, String next, String[] phone, List<String> output ){
+        if(next.isEmpty()){
+            output.add(combination);
+        }else{
+            String letters = phone[next.charAt(0) -'2'];
+            for (char letter: letters.toCharArray()){
+                backtrack(combination + letter, next.substring(1), phone, output);
+            }              
         }
-        
-        String letters = digitToLetters.get(digits.charAt(idx));
-        for (char letter : letters.toCharArray()) {
-            comb.append(letter);
-            backtrack(digits, idx + 1, comb, res, digitToLetters);
-            comb.deleteCharAt(comb.length() - 1);
-        }
-    }    
+    }
 }
