b62c9f988b0356cc16687b261c24383e02d37176
Yuvraj Singh <01yuvisingh@gmail.com>
2025-09-06 17:06:07 +0530

    Time: 13 ms (13.54%), Space: 45.2 MB (17.91%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,41 +1,18 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-         // In this approach, we will make a map that will take care of counting the elements and maintaining the frequency of each and every element as a unity by taking the latest index of every element.
-     HashMap < Character, Integer > mpp = new HashMap < Character, Integer > ();
-
-        int left = 0, right = 0;
-        int n = s.length();
-        int len = 0;
-        while (right < n) {
-            if (mpp.containsKey(s.charAt(right))) left = Math.max(mpp.get(s.charAt(right)) + 1, left);
-
-            mpp.put(s.charAt(right), right);
-
-            len = Math.max(len, right - left + 1);
-            right++;
+        int max =0;
+        int len =s.length();
+        for(int i=0 ; i<len; i++){
+            int[] freq = new int[128];
+            for(int j=i; j<len; j++){
+            char c= s.charAt(j);
+                freq[c]++;
+                if(freq[c]>=2){
+                    break;
+                }
+                max = Math.max(max, j-i+1);
+            }
         }
-        return len;
+        return max;
     }
-        
-        
-        // We will have two pointers left and right. Pointer ‘left’ is used for maintaining the starting point of substring while ‘right’ will maintain the endpoint of the substring.’ right’ pointer will move forward and check for the duplicate occurrence of the current element if found then ‘left’ pointer will be shifted ahead so as to delete the duplicate elements.
-    //      if(s.length()==0)
-    //          return 0;
-    //     int maxans = Integer.MIN_VALUE;
-    //     Set < Character > set = new HashSet < > ();
-    //     int l = 0;
-    //     for (int r = 0; r < s.length(); r++) // outer loop for traversing the string
-    //     {
-    //         if (set.contains(s.charAt(r))) //if duplicate element is found
-    //         {
-    //             while (l < r && set.contains(s.charAt(r))) {
-    //                 set.remove(s.charAt(l));
-    //                 l++;
-    //             }
-    //         }
-    //         set.add(s.charAt(r));
-    //         maxans = Math.max(maxans, r - l + 1);
-    //     }
-    //     return maxans;
-    // }
 }
